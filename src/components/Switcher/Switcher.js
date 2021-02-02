a3ec5f05225447a9a1b5bed09b1834ef3353ad3b
Vladimir <vladimirleontiev.devmail@gmail.com>
2021-02-02 12:23:48 +0300

    without grid

--- src/components/Switcher/Switcher.js
+++ src/components/Switcher/Switcher.js
@@ -1,9 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Switcher.module.css';
 
-const Switcher = ({value = true, yesText = 'is', noText = 'is not', clickHandler}) => {
+const Switcher = ({value = true, yesText = 'is', noText = 'is not', clickHandler, ...props}) => {
+  const [leftRef, setLeftRef] = useState(React.createRef());
+  const [rightRef, setRightRef] = useState(React.createRef());
+
   const [switcherValue, setSwitcherValue] = useState(value);
+  const [widthValue, setWidthValue] = useState('auto');
+  const widthStyle = {width: widthValue};
+
+  useEffect(() => {
+    const newWidth = Math.max(leftRef.current.offsetWidth, rightRef.current.offsetWidth);
+    setWidthValue(`${newWidth}px`);
+  }, []);
+
   const internalClickHandler = e => {
     setSwitcherValue(!switcherValue);
     if (clickHandler) clickHandler(e);
@@ -13,8 +24,12 @@ const Switcher = ({value = true, yesText = 'is', noText = 'is not', clickHandler
               onClick={internalClickHandler}
   >
     <div className={styles.background}>
-      <span className={`${styles.case} ${styles.unselectable}`}>{yesText}</span>
-      <span className={`${styles.case} ${styles.unselectable}`}>{noText}</span>
+      <span className={`${styles.case} ${styles.unselectable}`} ref={leftRef} style={widthStyle}>
+        {yesText}
+      </span>
+      <span className={`${styles.case} ${styles.unselectable}`} ref={rightRef} style={widthStyle}>
+        {noText}
+      </span>
       <div className={styles.slider}/>
     </div>
   </div>
